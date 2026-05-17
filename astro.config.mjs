a27e47d307490c34f1bf392dbdb43a0c037db1bf
I-AM-PHAROS <i_am_pharos@proton.me>
2026-05-17 07:34:03 +0300

    configure base for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://i-am-pharos.github.io',
+  base: '/I-AM-1/',
+  output: 'static'
+});
